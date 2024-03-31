07c993feb0cbf9acc4462546c94e72e37955da89
Nevin Leh <nevinleh@proton.me>
2024-03-31 16:18:54 -0600

    Teensy bit of progress

--- src/main.c
+++ src/main.c
@@ -3,47 +3,52 @@
 #include <string.h>
 // #include "qfclogger.h"
 struct chars {
-    ssize_t size;
-    char* chars;
+  ssize_t size;
+  char *chars;
 };
 
 struct chars input_file;
 
-void qfcReadFile(char* fileName) {
-    FILE* fptr = fopen(fileName, "r");
-    if (!fptr) {
-        printf("Failed to open %s", fileName);
-    }
+void qfcReadFile(char *fileName) {
+  FILE *fptr = fopen(fileName, "r");
+  if (!fptr) {
+    printf("Failed to open %s", fileName);
+  }
 
-    // full file as an array
-    input_file.chars = NULL;
-    input_file.size = 0;
+  // full file as an array
+  input_file.chars = NULL;
+  input_file.size = 0;
 
-    // used for reading in lines
-    char* line = NULL;
-    size_t len = 0;
-    ssize_t line_len = 0;
+  // used for reading in lines
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t line_len = 0;
 
-    while ((line_len = getline(&line, &len, fptr)) != -1) {
-        input_file.chars =
-            realloc(input_file.chars, input_file.size + line_len);
-        memcpy(&input_file.chars[input_file.size], line, line_len);
-        input_file.size += line_len;
-    }
-    fclose(fptr);
+  while ((line_len = getline(&line, &len, fptr)) != -1) {
+    input_file.chars = realloc(input_file.chars, input_file.size + line_len);
+    memcpy(&input_file.chars[input_file.size], line, line_len);
+    input_file.size += line_len;
+  }
+  free(line);
+  fclose(fptr);
 
-    // terminate string
-    input_file.size += 1;
-    input_file.chars = realloc(input_file.chars, input_file.size);
-    input_file.chars[input_file.size - 1] = '\0';
-
-    printf(input_file.chars);
+  // terminate string
+  input_file.size += 1;
+  input_file.chars = realloc(input_file.chars, input_file.size);
+  input_file.chars[input_file.size - 1] = '\0';
 }
-int main(int argc, char* argv[]) {
-    if (argc >= 2) {
-        qfcReadFile(argv[1]);
-    } else {
-        printf("You must provide a filename as an argument");
-    }
-    return 0;
+
+// Just getting pointer to start of layer for now
+void extractLayers(char *input) {
+  char *startPtr = strstr(input, "\"layers\"");
+  printf(startPtr);
+}
+int main(int argc, char *argv[]) {
+  if (argc >= 2) {
+    qfcReadFile(argv[1]);
+  } else {
+    printf("You must provide a filename as an argument");
+  }
+  extractLayers(input_file.chars);
+  return 0;
 }
