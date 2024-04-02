55f6a3471b89045a817d618bdcc1e153c7a51c42
Nevin Leh <nevinleh@proton.me>
2024-04-02 13:56:09 -0600

    Have individual arrays parsed out

--- src/QJTCP/QJTCP.c
+++ src/QJTCP/QJTCP.c
@@ -5,18 +5,24 @@
 #include <string.h>
 // #include "qfclogger.h"
 
-// I know this is probably not a good idea. Just having some fun with macros
-#define TIMEOUT_LEN 30000
-#define RESET_TIMEOUT() (timeout = 0)
-#define TIMED_OUT ((timeout > TIMEOUT_LEN && timeout++))
-unsigned long timeout = 0;
-
+#define MAX_LAYERS 20
 struct chars {
   ssize_t size;
   char *chars;
 };
 
+typedef struct substring {
+  ssize_t startIndex;
+  ssize_t endIndex;
+} substring;
+
+typedef struct layers {
+  char **layers;
+} layers;
+
 struct chars input_file;
+
+layers kb_layers;
 
 void qfcReadFile(char *fileName) {
   FILE *fptr = fopen(fileName, "r");
@@ -60,18 +66,34 @@ char *extractLayers(char *input) {
     printf("Json malformed\n");
     return NULL;
   }
-
   int bracket_cntr = 1;
   size_t len = 1;
 
-  // TODO: Fix segfault here
-  RESET_TIMEOUT();
+  substring *layers = NULL;
+  size_t num_layers = 0;
+  bool in_layer = false;
+
   while (bracket_cntr != 0 && startPtr[len] != '\0') {
     if (startPtr[len] == '[') {
       bracket_cntr += 1;
     } else if (startPtr[len] == ']') {
       bracket_cntr -= 1;
     }
+
+    if (bracket_cntr > 2) {
+      printf("Malformed layers element. Structure too deep\n");
+    }
+
+    if (!in_layer && bracket_cntr == 2) {
+      in_layer = true;
+      num_layers += 1;
+      layers = realloc(layers, sizeof(substring) * num_layers);
+      layers[num_layers - 1].startIndex = len;
+    } else if (in_layer && bracket_cntr == 1) {
+      in_layer = false;
+      layers[num_layers - 1].endIndex = len;
+    }
+
     len += 1;
   }
 
@@ -80,10 +102,29 @@ char *extractLayers(char *input) {
     return NULL;
   }
 
-  char *layers = malloc(len + 1);
-  strncpy(layers, startPtr, len);
-  layers[len] = '\0';
-  return layers;
+  kb_layers.layers = malloc(sizeof(char *) + num_layers);
+
+  unsigned long i = 0;
+  for (i = 0; i < num_layers; i++) {
+    kb_layers.layers[i] = malloc(layers[i].endIndex - layers[i].startIndex + 2);
+    kb_layers.layers[i] =
+        memcpy(kb_layers.layers[i], &startPtr[layers[i].startIndex],
+               layers[i].endIndex - layers[i].startIndex + 1);
+    kb_layers.layers[i][layers[i].endIndex - layers[i].startIndex + 1] = '\0';
+  }
+
+  // debugging printing. Delete
+  for (i = 0; i < num_layers; i++) {
+    printf("++++++++++++++++++++++++++++++++\n");
+    printf(kb_layers.layers[i]);
+    printf("++++++++++++++++++++++++++++++++\n");
+  }
+
+  // Remove rest of file. This is old. Need to fix unit tests
+  char *layers1 = malloc(len + 1);
+  strncpy(layers1, startPtr, len);
+  layers1[len] = '\0';
+  return layers1;
 }
 
 void run(char *filename) {
